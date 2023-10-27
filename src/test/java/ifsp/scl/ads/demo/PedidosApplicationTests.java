724e4a166ecf311f5c1082a2a04e27f7dcf85643
Jean Cremonezzi <jeancremonezzidev@gmail.com>
2023-10-27 15:10:57 -0300

    Initial commit

---
+++ src/test/java/ifsp/scl/ads/demo/PedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsp.scl.ads.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
